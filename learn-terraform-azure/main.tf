224329eef0447ed6c57c813b3a55c16110c52afe
Your Name <you@example.com>
2022-05-04 15:06:15 +0300

    Initial commit terraform

---
+++ learn-terraform-azure/main.tf
@@ -0,0 +1,20 @@
+# Configure the Azure provider
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.0.2"
+    }
+  }
+
+  #equired_version = ">= 1.1.8"
+}
+
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "rg" {
+  name     = "myTFResourceGroup"
+  location = "Frankfurt"
+}
